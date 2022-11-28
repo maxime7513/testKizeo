bf138e10e85c01978be89e295b407c928b3e9ac2
maxime7513 <makitoo@outlook.fr>
2022-11-28 13:36:17 +0100

    simulateur module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardProductComponent } from './components/card-product/card-product.component';
+import { ProductsComponent } from './components/products/products.component';
+import { PanierComponent } from './components/panier/panier.component';
+import { SimulateurComponent } from './components/simulateur/simulateur.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardProductComponent,
+    ProductsComponent,
+    PanierComponent,
+    SimulateurComponent
   ],
   imports: [
     BrowserModule,
